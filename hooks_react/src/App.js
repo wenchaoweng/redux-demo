67ba7ebb88ed3f7b0505d2125c8d5afbd32db5a1
wenchao <397385020@qq.com>
2020-02-15 15:06:06 +0800

    use hash to route page

--- hooks_react/src/App.js
+++ hooks_react/src/App.js
@@ -1,23 +1,46 @@
 import React from 'react';
 // 1. 引入useState
 import { useState, useEffect } from 'react';
+import "./App.css"
+
+function Box1() {
+  return (
+    <div className="box">
+      注册
+    </div>
+  )
+}
+
+function Box2() {
+  return (
+    <div className="box">
+      登录
+    </div>
+  )
+}
 
 function App() {
-  // 2. 使用useState给count和setCount初始化
-  const [count/*值*/, setCount/*更新函数*/] = useState(0);
-  useEffect(()=>{
-    document.querySelector("#output").innerText = count
-  })
-  const add = () => {
-    // 3. 调用setCount给count赋值
-    setCount(count + 1)
+  //1. 获取浏览器的地址hash，如http://localhost:3001/#signup中的#signup即为hash
+  let hash = window.location.hash;
+  //2. 如果hash不为#signup，默认显示登录界面
+  let initUi = hash === "#signup" ? "注册" : "登录";
+  const [ui, setUi] = useState(initUi);
+  //3. 点击登录，设置ui变量为登录，同时修改浏览器的地址hash为login
+    const onClickLogin = () => {
+    setUi("登录");
+    window.location.hash = "login";
   }
- 
+
+  const onClickSignUp = () => {
+    setUi("注册");
+    window.location.hash = "signup";
+  }
 
   return (
-    <div>
-      <div>{count}</div>
-      <div><button onClick={add}>+1</button></div>
+    <div className="App">
+      <button onClick={onClickLogin}>登录</button>
+      <button onClick={onClickSignUp}>注册</button>
+      <div>{ui === "注册" ? <Box1 /> : <Box2 />}</div>
     </div>
   )
 }
