ecf9c8d2675be0b0fa0b318ea7ab2e8b880e0667
wenchao <397385020@qq.com>
2020-02-15 20:57:36 +0800

    use react lifecycle api

--- hooks_react/src/App.js
+++ hooks_react/src/App.js
@@ -1,50 +1,102 @@
 import React from "react";
-import {  BrowserRouter as Router,  Switch,  Route,  Link} from "react-router-dom";
+import ReactDOM from "react-dom";
+import "./App.css";
 
-export default function App() {
-  return (
-    <Router>
+let div = document.createElement("div");
+document.body.appendChild(div);
+console.log = function(content) {
+  div.innerHTML += `${content}<br>`;
+};
+
+class Baba extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hasChild: true
+    };
+  }
+  onClick() {
+    this.setState({
+      hasChild: false
+    });
+  }
+  callSon() {
+    this.setState({
+      word: "你还好吧"
+    });
+  }
+  render() {
+    return (
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        我是你爸爸
+        <button onClick={() => this.onClick()}>kill son</button>
+        <button onClick={() => this.callSon()}>call son</button>
+        {this.state.hasChild ? <App word={this.state.word} /> : null}
       </div>
-    </Router>
-  );
+    );
+  }
 }
 
-function Home() {
-  return <h2>Home</h2>;
+class App extends React.Component {
+  onClick() {
+    console.log("用户点击了");
+    this.setState({
+      n: this.state.n + 1
+    });
+  }
+  updateX() {
+    this.setState({
+      x: this.state.x + "!"
+    });
+  }
+  constructor() {
+    super();
+    this.state = {
+      n: 0,
+      x: "不展示"
+    };
+  }
+  componentWillMount() {
+    console.log("将要 mount App");
+  }
+  render() {
+    // update
+    console.log("填充/更新 App 的内容");
+    return (
+      <div className="App">
+        {this.state.n}
+        <br />
+        我爸说 {this.props.word}
+        <br />
+        <button onClick={() => this.onClick()}>+1</button>
+        <button onClick={() => this.updateX()}>update x</button>
+      </div>
+    );
+  }
+  componentDidMount() {
+    console.log("mount App 完毕");
+  }
+  componentWillUpdate() {
+    console.log("update App 之前");
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("要不要更新呢？");
+    if (this.state.n === nextState.n) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  //update is render
+  componentDidUpdate() {
+    console.log("update App 之后");
+  }
+  componentWillUnmount() {
+    console.log("App 快要死了，记得喂狗");
+  }
+  componentWillReceiveProps() {
+    console.log("我爸说话了");
+  }
 }
 
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
+ export default Baba;
